9883299a7813295483226679d9ab6a2abfe6abbe
FluffyQQ <fluffytime@Yandex.ru>
2023-03-13 07:25:28 +0300

    текстовый элемент (Label) и кнопка (Button) которая добавляет +1 к числу и отображается в Label

--- practicum-counter/ViewController.swift
+++ practicum-counter/ViewController.swift
@@ -8,12 +8,29 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    private var counter = 0
+    
+    @IBOutlet weak var counterLabel: UILabel! //счетчик
+    @IBOutlet weak var counterButton: UIButton! //Кнопка
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        updateCounterLabel()
+        setupButton()
+    }
+    
+    private func setupButton() {
+        counterButton.layer.cornerRadius = 5
+        counterButton.layer.masksToBounds = true
     }
 
-
+    private func updateCounterLabel() {
+        counterLabel.text = "Значение счётчика: \(counter)"
+    }
+    
+    @IBAction func buttonDidTap(_ sender: Any) {
+        counter += 1
+        updateCounterLabel()
+    }
 }
-
