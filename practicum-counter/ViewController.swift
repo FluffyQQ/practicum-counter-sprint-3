8563aaa36a5bf25d0b7fef3fd67670de2b085ddc
FluffyQQ <fluffytime@Yandex.ru>
2023-03-13 07:04:57 +0300

    Initial Commit

---
+++ practicum-counter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  practicum-counter
+//
+//  Created by Максим Фролов on 13.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
